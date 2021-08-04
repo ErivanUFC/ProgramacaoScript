d21cc0c4774efe388e1d67207b705088099a916d
Erivan Pereira <erivan@alu.ufc.br>
2021-08-04 11:35:21 -0300

    elevar numero

--- index.js
+++ index.js
@@ -3,10 +3,21 @@ const path = require('path')
 const app = express();
 const PORT = process.env.PORT || 5000
 
-app.use(express.static('public'));
+app.use(express.urlencoded({
+  extended: true
+}))
+
+app.set('view engine', 'ejs');
+
+app.post('/', (req, res) => {
+  let number = req.body.number;
+  let result = Math.pow(number, 2);
+
+  res.render('index', { result: result });
+});
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+  res.render('index', { result: undefined });
 });
 
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
